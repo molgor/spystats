644c5272050ff70fc46e80807b48f1f2a3c859e9
Escamilla Molgora  Juan <escamill@wayland.private.dns.zone>
2017-09-29 19:56:37 +0100

    Fx something in the hec version

--- HEC_runs/fit_fia_sppn.py
+++ HEC_runs/fit_fia_sppn.py
@@ -25,7 +25,8 @@ __email__ ="molgor@gmail.com"
 import geopandas as gpd
 from shapely.geometry import Point
 import pandas as pd
-import GPflow as gf
+#import GPflow as gf
+import gpflow as gf
 import numpy as np
 
 import logging
