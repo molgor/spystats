6040e286138fbfb80cbc327e197149de1ee4ad97
Juan Escamilla <molgor@gmail.com>
2018-11-29 21:36:05 +0000

    Test

--- biospytial_models/runner.py
+++ biospytial_models/runner.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+"""
+Runner file to be used in HEC
+"""
+
+
+__author__ = "Juan Escamilla Mólgora"
+__copyright__ = "Copyright 2018, JEM"
+__license__ = "GPL"
+__mantainer__ = "Juan"
+__email__ ="j.escamilla.molgora@ciencias.unam.mx"
+
 #import traversals.strategies as st
 #from os import walk
 import matplotlib.pyplot as plt
@@ -8,84 +23,97 @@ import pymc3 as pm
 import sys
 import redis
 import pickle
+import logging
 from patsy import dmatrices,dmatrix
+#import ipdb; ipdb.set_trace()
 
-from spystats import utilities as ut
+#from spystats import utilities as ut
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 
 def ModelSamplingEffort(trainDM,PredDM):
     """
     Sampling effort model
     parameters:
         trainDM : (duple of dataframes). loc 0 is the lhs term of the equation.
-        PredDM : dataframe 
+        PredDM : dataframe
+        pymc_model : A Pymc3 model object
 
     notes:
         Should be the output of the patsy.dmatrices and dmatrix respectively
     """
     with pm.Model() as model:
 
-       	# partition dataframes df
+        # partition dataframes df
         Ydf = trainDM[0]
-       	TXdf = trainDM[1]
-       
+        TXdf = trainDM[1]
+
         PXdf = PredDM
-       
-		## Parameters for linear predictor
-	        #b0 = pm.Normal('b0',mu=0,sd=10)
-	        #dum_names = filter(lambda col : str(col).startswith('inegiv5name'),TXdf)
-	        #dumsdf = TXdf[dum_names]
-	        #dumshape = dumscols.shape
-	        #coordsdf = TXdf[['Longitude','Latitude']]
-        
-	        # Create vectors for dumi vars 
-	        #drvs = map(lambda col : pm.Normal(col,mu=0,sd=1.5),dum_names)
-	        ## Create theano vector
+
+        ## Parameters for linear predictor
+        #b0 = pm.Normal('b0',mu=0,sd=10)
+        #dum_names = filter(lambda col : str(col).startswith('inegiv5name'),TXdf)
+        #dumsdf = TXdf[dum_names]
+        #dumshape = dumscols.shape
+        #coordsdf = TXdf[['Longitude','Latitude']]
+
+        # Create vectors for dumi vars 
+        #drvs = map(lambda col : pm.Normal(col,mu=0,sd=1.5),dum_names)
+        ## Create theano vector
         dimX = len(TXdf.columns)
         b = pm.Normal('b',mu=0,sd=1.5,shape=dimX)
-	        #mk = pm.math.matrix_dot(TXdf.values,b.transpose())
-        
-       
-        
-	        ## The latent function
+        #mk = pm.math.matrix_dot(TXdf.values,b.transpose())
+
+
+
+        ## The latent function
         x_index = TXdf.columns.get_loc(b"Longitude")
         y_index = TXdf.columns.get_loc(b"Latitude")
-        
-	        ## Building the covariance structure
+
+        ## Building the covariance structure
         tau = pm.HalfNormal('tau',sd=10)
         sigma = pm.HalfNormal('sigma',sd=10)
-	        #phi = pm.Uniform('phi',0,15)
+        #phi = pm.Uniform('phi',0,15)
         phi = pm.HalfNormal('phi',sd=6)
         Tau = pm.gp.cov.Constant(tau)
         cov = (sigma * pm.gp.cov.Matern32(2,phi,active_dims=[x_index,y_index])) + Tau
-       
-       	mean_f = pm.gp.mean.Linear(coeffs=b)
-        
+
+        mean_f = pm.gp.mean.Linear(coeffs=b)
+
         gp = pm.gp.Latent(mean_func=mean_f,cov_func=cov)
-        
+
         f = gp.prior("latent_field", X=TXdf.values,reparameterize=False)
-        
-
-       	yy = pm.Bernoulli("yy",logit_p=f,observed=Ydf.values)
 
 
+        yy = pm.Bernoulli("yy",logit_p=f,observed=Ydf.values)
 
-	        #trace = pm.fit(method='advi', callbacks=[CheckParametersConvergence()],n=15000)    
+
+
+        #trace = pm.fit(method='advi', callbacks=[CheckParametersConvergence()],n=15000)    
         trace = pm.sample(150,init='adapt_diag')
-	        #trace = trace.sample(draws=5000)
-        
-	        # Remove any column that doesnt appear in the training data
+        #trace = trace.sample(draws=5000)
+
+        # Remove any column that doesnt appear in the training data
         ValidPreds = PredDM[TXdf.columns]
         PredX = ValidPreds.values
-    
-       	f_star = gp.conditional("f_star", PredX)
+
+        f_star = gp.conditional("f_star", PredX)
 
         pred_samples = pm.sample_ppc(trace, vars=[f_star], samples=100)
-        return pred_samples,trace
+
+    return pred_samples,trace
+
+	        #trace = pm.fit(method='advi', callbacks=[CheckParametersConvergence()],n=15000)    
+
 
 def loadDataFrameFromRedis(keyname,redis_connection):
     """
     Loads a Pandas dataframe stored in redis given a key
     """
+    logger.info("Loading data from RedisDB")
     return pickle.loads(redis_connection.get(keyname))
 
 
@@ -115,23 +143,31 @@ def splitByFormula(formula,traindf,predictordf):
         traindf : (pandas.DataFrame) The dataframe for training.
         predictordf : (pandas.DataFrame) the dataframe for prediction
     """
-    
+    logger.info("Creating Design matrices from the formula %s"%formula)
     lhs,rhs = formula.split('~')
     TM = dmatrices(formula ,traindf,return_type="dataframe")
     PM = dmatrix(rhs,predictordf,return_type='dataframe')
     return (TM,PM)
 
+def main():
+    panthera = '10.42.72.93'
+    conn = redis.StrictRedis(host=panthera,password='biospytial.')
+    predkey = 'p-50x50-guerrero-4'
+    trainkey = 't-luca-guerrero-4'
+    PDF = preparePredictors(loadDataFrameFromRedis(predkey,conn))
+    TDF = loadDataFrameFromRedis(trainkey,conn)
 
-panthera = '10.42.72.93'
-conn = redis.StrictRedis(host=panthera,password='biospytial.')
+    formula = 'LUCA ~ Longitude + Latitude + Q("Dist.to.road_m") + Population_m + name'
 
-predkey = 'p-50x50-guerrero-4'
-trainkey = 't-luca-guerrero-4'
-PDF = preparePredictors(loadDataFrameFromRedis(predkey,conn))
-TDF = loadDataFrameFromRedis(trainkey,conn)
+    TM,PM = splitByFormula(formula,TDF,PDF['clean'])
+    logger.info("Start modelling inference")
 
+    modelo = ModelSamplingEffort(TM,PM)
 
-formula = 'LUCA ~ Longitude + Latitude + Q("Dist.to.road_m") + Population_m + name'
+    conn.set('test-model',pickle.dumps(modelo))
+    logger.info("Finish!")
 
-TM,PM = splitByFormula(formula,TDF,PDF['clean'])
+if __name__ == '__main__':
+    
+    main()
 
